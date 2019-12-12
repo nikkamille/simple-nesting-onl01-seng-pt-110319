72d886690e429df35a6bed9ee53f4c75c0763cd1
Kamille Longalong <k.pagibigan@gmail.com>
2019-12-12 07:02:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,8 +106,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = programmer_hash[:alan_kay][:known_for] = "GUI"
-     puts alans_new_info
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     
      
 end
 
